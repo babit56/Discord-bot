b47360b9eaead9809157ecf03725ec1bf1c3c580
= <=>
2021-10-11 14:38:17 +0200

    testing for pull request

--- commands/join.js
+++ commands/join.js
@@ -1,10 +1,12 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
+const ytdl = require("ytdl-core");
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('join')
-        .setDescription('Joins your voice channel. Won\'t work f I\'m already in a vc. Administrator permissions overrides this'),
+        .setDescription(`Joins your voice channel. 
+            Won't work f I'm already in a vc. Administrator permissions overrides this`),
     execute(interaction) {
-        console.log('joining lmao');
+        
     }
 }
